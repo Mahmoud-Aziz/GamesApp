4e2368d1f37f190fecaec36bf3c2ccafe7bf6293
“Mahmoud <mahmoudmohammedaziz@gmail.com>
2022-03-30 22:58:23 +0200

    Initial root view controller.
    - Create and set gamesViewController as the rootVC.
    
    - Remove storyboard and sceneDelegate and set the rootVC from appDelegate.

---
+++ Features/Games/GamesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GamesViewController.swift
+//  GamesApp
+//
+//  Created by Mahmoud Aziz on 30/03/2022.
+//
+
+import UIKit
+
+class GamesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
