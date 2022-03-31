63718a2bc38f9b60f881ef07c0909a4eca853482
“Mahmoud <mahmoudmohammedaziz@gmail.com>
2022-03-31 23:56:55 +0200

    Add network layer.
    - RequestBuilder: Depends on builder pattern to construct the URLRequest.
    
    - APIService: Depends on URLSession datatask to construct the URLRequest.
    
    - Types group: Includes endpoint, methods, errors, baseurl.
    
    TODO: move baseurl to xcconfig file.

---
+++ Shared/Networking/APIBuilder/RequestBuilder.swift
@@ -0,0 +1,59 @@
+//
+//  RequestBuilder.swift
+//  GamesApp
+//
+//  Created by Mahmoud Aziz on 31/03/2022.
+//
+
+import Foundation
+
+class RequestBuilder {
+    private var baseURL: URL?
+    private var endpoint: String?
+    private var method: HTTPMethod = .get
+    private var headers: [String: String] = [:]
+    private var parameters: [String: String] = [:]
+    private var httpBodyParams: Data?
+    // TODO: after finish setup for base url and config file
+     
+    func setBaseUrl(_ string: String) {
+        baseURL = URL(string: string) ?? URL(string: "")
+    }
+    func setEndpoint(_ value: String) {
+        endpoint = value
+    }
+    func setMethod(_ value: HTTPMethod) {
+        method = value
+    }
+    func setHeader(_ key: String, _ value: String) {
+        headers[key] = value
+    }
+    func setQueryParameters(_ key: String, _ value: String) {
+        parameters[key] = value
+    }
+    func setBodyParameters<T: Encodable>(_ body: T) {
+        var httpBody: Data?
+        do {
+            let httpbodyData = try JSONSerialization.data(withJSONObject: body.toDictionary(), options: [])
+            httpBody = httpbodyData } catch {
+            //TODO: Log, after adding logging utility
+        }
+        httpBodyParams = httpBody
+    }
+    func build() -> URLRequest {
+        guard let endpoint = endpoint, let baseURL = baseURL else {
+            //TODO: REFACTOR
+            return URLRequest(url: URL(string: "")!)
+        }
+        let url = baseURL.appendingPathComponent(endpoint)
+        var components = URLComponents(string: url.absoluteString)
+        components?.queryItems = parameters.compactMap { URLQueryItem(name: $0.key, value: $0.value) }
+        var urlRequest = URLRequest(url: components!.url!)
+        urlRequest.httpMethod = method.rawValue
+        urlRequest.httpBody = httpBodyParams
+        for header in headers {
+            urlRequest.addValue(header.value, forHTTPHeaderField: header.key)
+        }
+        return urlRequest
+    }
+}
---
+++ Shared/Networking/Types/HTTPMethod.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPMethod.swift
+//  GamesApp
+//
+//  Created by Mahmoud Aziz on 31/03/2022.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get
+    case post
+}
