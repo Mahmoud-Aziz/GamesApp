9df5151a615e5bfd4f8819b21be743a6e03757ab
“Mahmoud <mahmoudmohammedaziz@gmail.com>
2022-04-02 01:32:49 +0200

    Add search use case.

--- Features/Games/Domain/DataProvider/GamesDataProvider.swift
+++ Features/Games/Domain/DataProvider/GamesDataProvider.swift
@@ -6,8 +6,11 @@
 //
 
 import Foundation
-typealias GamesResultHandler = Result<[GamesResponse], Error>
+typealias GamesResultHandler = Result<GamesResponse, Error>
 typealias GamesReponseHandler = (GamesResultHandler) -> Void
+typealias SearchResultHandler = Result<SearchResponse, Error>
+typealias SearchReponseHandler = (SearchResultHandler) -> Void
+
 
 class GamesDataProvider: GamesUseCase {
     func getGames(completion: @escaping GamesReponseHandler) {
@@ -25,6 +28,27 @@ class GamesDataProvider: GamesUseCase {
                 completion(.success(value))
             case .failure(let error):
                 // Since this coming from previous layer, it should be already logged.
+                completion(.failure(error))
+            }
+        })
+    }
+    
+    func search(for game: String, completion: @escaping SearchReponseHandler) {
+        let requestBuilder = RequestBuilder()
+        requestBuilder.setBaseUrl(APIBaseURL.baseURL)
+        requestBuilder.setEndpoint(APIEndpoint.games.rawValue)
+        requestBuilder.setMethod(.get)
+        requestBuilder.setQueryParameters("page_size", "10")
+        requestBuilder.setQueryParameters("key", APIKey.apiKey)
+        requestBuilder.setQueryParameters("page", "1")
+        requestBuilder.setQueryParameters("search", "\(game)")
+        let request = requestBuilder.build()
+        
+        APIService.shared.request(decodable: SearchResponse.self, request: request, completion: { result in
+            switch result {
+            case .success(let result):
+                completion(.success(result))
+            case .failure(let error):
                 completion(.failure(error))
             }
         })
