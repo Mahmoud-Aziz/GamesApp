cc6bbe53ce28940c930c6a1fecaacebebab3ffc4
“Mahmoud <mahmoudmohammedaziz@gmail.com>
2022-04-02 22:15:45 +0200

    Replace tabbar with a custom one and update rootVC in appdelegate.

--- SupportingFiles/AppDelegate.swift
+++ SupportingFiles/AppDelegate.swift
@@ -8,16 +8,15 @@
 import UIKit
 import netfox
 
-
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         NFX.sharedInstance().start() 
         window = UIWindow(frame: UIScreen.main.bounds)
-        let rootViewController = GamesViewController()
+        let rootViewController = TabBarViewController()
         let navigationController = UINavigationController(rootViewController: rootViewController)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
