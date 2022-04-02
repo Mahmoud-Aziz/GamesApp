6b70c8a8260bb4144c7bcfb07eaceecbd996b73f
“Mahmoud <mahmoudmohammedaziz@gmail.com>
2022-04-02 19:06:30 +0200

    Add GamesRoute to handle navigation to details screen.

---
+++ Features/Games/Presentation/NavigationRouter/GamesRoutes.swift
@@ -0,0 +1,28 @@
+//
+//  GamesRoutes.swift
+//  GamesApp
+//
+//  Created by Mahmoud Aziz on 02/04/2022.
+//
+
+import UIKit
+
+enum GamesRoutes: Route {
+    case gameDetails(Response)
+
+    var destination: UIViewController {
+        switch self {
+        case .gameDetails(let game):
+            let detailsViewModel = DetailsViewModel(dataSource: DetailsDataProvider(id: game.id))
+            let detailsViewController = DetailsViewController(viewModel: detailsViewModel)
+            return detailsViewController
+        }
+    }
+
+    var style: NaivgationStyle {
+        switch self {
+        case .gameDetails:
+            return .push
+        }
+    }
+}
