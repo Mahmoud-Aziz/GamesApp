db2bcf2fc821c83b48a3706eb17f2d9f559fa624
“Mahmoud <mahmoudmohammedaziz@gmail.com>
2022-04-02 19:05:57 +0200

    Update details ViewModel.

--- Features/Details/Presentaion/ViewModel/DetailsViewModel.swift
+++ Features/Details/Presentaion/ViewModel/DetailsViewModel.swift
@@ -7,6 +7,68 @@
 
 import Foundation
 
-class DetailsViewModel {
+protocol DetailsViewModelProtocol {
+    func getTitle() -> String
+    func getDetails()
+    func viewDidLoad(statePresenter: StatePresentable)
+    func getDescription() -> String
+    func getReddit() -> String
+    func getWebsite() -> String
+    func getImage() -> String
+}
+
+class DetailsViewModel: DetailsViewModelProtocol {
+    private let dataSource: DetailsUseCase
+    private var gameDetails: DetailsResponse?
+    var state: StatePresentable?
     
+    init(dataSource: DetailsUseCase) {
+        self.dataSource = dataSource
+    }
+    
+    func viewDidLoad(statePresenter: StatePresentable) {
+        self.state = statePresenter
+        self.state?.render(state: .initial)
+        getDetails()
+    }
+
+    func getDetails() {
+        dataSource.getDetails(completion: { [weak self] result in
+            switch result {
+            case .success(let details):
+                self?.gameDetails = details
+                self?.state?.render(state: .loaded)
+            case .failure(let error):
+                self?.state?.render(state: .error(NetworkError.failedRequest.rawValue))
+                //TODO: Log error 
+            }
+        })
+    }
+    
+    func getTitle() -> String {
+        guard let title = gameDetails?.name else { return "-" }
+        return title
+    }
+    
+    func getDescription() -> String {
+        guard let description = gameDetails?.description else { return "-" }
+        let symbols = ["<p>":"", "</p>":"","<br />":""]
+        let clearDescription = symbols.reduce(description) { $0.replacingOccurrences(of: $1.key, with: $1.value) }
+        return clearDescription
+    }
+    
+    func getReddit() -> String {
+        guard let reddit = gameDetails?.redditUrl else { return "-" }
+        return reddit
+    }
+    
+    func getWebsite() -> String {
+        guard let website = gameDetails?.website else { return "-" }
+        return website
+    }
+    
+    func getImage() -> String {
+        guard let image = gameDetails?.backgroundImage else { return ViewError.alert.rawValue }
+        return image
+    }
 }
