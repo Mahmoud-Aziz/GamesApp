6c293dbd20ce3fbb30d6c59692b707dfbb83950c
“Mahmoud <mahmoudmohammedaziz@gmail.com>
2022-04-03 04:50:09 +0200

    Re add project to fix cloning issue :angry:

--- GamesApp/SupportingFiles/AppDelegate.swift
+++ GamesApp/SupportingFiles/AppDelegate.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import netfox
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        NFX.sharedInstance().start() 
         window = UIWindow(frame: UIScreen.main.bounds)
         let rootViewController = TabBarViewController()
         let navigationController = UINavigationController(rootViewController: rootViewController)
