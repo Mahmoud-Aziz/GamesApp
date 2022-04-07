1e92674d1f1ff3397ade459d60b065416756420d
“Mahmoud <mahmoudmohammedaziz@gmail.com>
2022-04-07 10:34:45 +0200

    Fix folder path.

---
+++ GamesApp/Shared/Footer/FooterReusableCollection.swift
@@ -0,0 +1,30 @@
+//
+//  FooterReusableCollection.swift
+//  GamesApp
+//
+//  Created by Mahmoud Aziz on 07/04/2022.
+//
+
+import Foundation
+
+import UIKit
+let footerReuseIdentifier = "FooterReusableCollection"
+class FooterReusableCollection: UICollectionReusableView {
+    private let activityIndicator = UIActivityIndicatorView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+     }
+     required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+     }
+    
+    private func configure() {
+        activityIndicator.color = .red
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.startAnimating()
+        activityIndicator.center = self.center
+        addSubview(activityIndicator)
+    }
+}
