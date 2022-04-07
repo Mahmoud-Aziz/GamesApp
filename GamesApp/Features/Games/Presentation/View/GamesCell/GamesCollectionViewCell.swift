a03f36aae13a4565c02ad0cea420c76e53f00628
“Mahmoud <mahmoudmohammedaziz@gmail.com>
2022-04-07 09:00:03 +0200

    Improve loading images using Nuke.

--- GamesApp/Features/Games/Presentation/View/GamesCell/GamesCollectionViewCell.swift
+++ GamesApp/Features/Games/Presentation/View/GamesCell/GamesCollectionViewCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Nuke
 
 class GamesCollectionViewCell: UICollectionViewCell {
     @IBOutlet private weak var gameTitle: UILabel!
@@ -13,15 +14,23 @@ class GamesCollectionViewCell: UICollectionViewCell {
     @IBOutlet private weak var metacriticScore: UILabel!
     @IBOutlet private weak var gameImageView: UIImageView!
     
+    private var task: ImageTask?
+    
     var game: Response? {
         didSet {
             guard let game = game else { return }
             setupCell(game: game)
         }
     }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        //Cancel task when cell is prepared for reuse to handle wrong images issue.
+        task?.cancel()
+    }
 }
 
-//MARK: - Cell data:
+//MARK: - Setup cell data:
 private extension GamesCollectionViewCell {
     func setupCell(game: Response) {
         gameTitle.text = game.name
@@ -46,15 +55,31 @@ private extension GamesCollectionViewCell {
         metacriticScore.text = score
     }
     
+    /// Setup image using Nuke's ImagePipeLine to load images in background thread.
     func setImage(game: Response) {
-        guard let imageURL = game.backgroundImage else { return }
-        gameImageView.setImage(url: imageURL, placeHolder: Images.placeholder.image)
+        guard let imageURL = game.backgroundImage?.toURL else { return }
+        gameImageView.image = ImageLoadingOptions.shared.placeholder
+        gameImageView.contentMode = .scaleAspectFit
+        
+        let task = ImagePipeline.shared.loadImage(with: imageURL) { [weak self] response in
+            guard let self = self else {
+                return
+            }
+            switch response {
+            case .failure:
+                self.gameImageView.image = ImageLoadingOptions.shared.failureImage
+                self.gameImageView.contentMode = .scaleAspectFit
+            case let .success(imageResponse):
+                self.gameImageView.image = imageResponse.image
+                self.gameImageView.contentMode = .scaleAspectFill
+            }
+        }
+        self.task = task
     }
     
     enum placeholder: String {
         case title = "No available name"
         case genre = "No available genre"
         case score = "No available score"
-        case image = "placeholder"
     }
 }
