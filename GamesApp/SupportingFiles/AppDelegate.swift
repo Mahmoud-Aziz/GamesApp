c1153805fb45f2812a7e3352a83bc96d428fe0d4
“Mahmoud <mahmoudmohammedaziz@gmail.com>
2022-04-07 08:59:18 +0200

    Remove netfox and setup tab bar view controller title to "Games".

--- GamesApp/SupportingFiles/AppDelegate.swift
+++ GamesApp/SupportingFiles/AppDelegate.swift
@@ -6,16 +6,17 @@
 //
 
 import UIKit
-import netfox
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        NFX.sharedInstance().start()
         window = UIWindow(frame: UIScreen.main.bounds)
         let rootViewController = TabBarViewController()
+        rootViewController.navigationItem.title = "Games"
+        rootViewController.navigationController?.navigationBar.prefersLargeTitles = true
+        rootViewController.navigationController?.navigationItem.largeTitleDisplayMode = .always
         let navigationController = UINavigationController(rootViewController: rootViewController)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
